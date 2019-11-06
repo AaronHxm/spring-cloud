3192f938dc0fe0c0381d0b248dad52a5ea5a76e4
CrazyAaronM <78032724@qq.com>
2019-11-06 17:07:44 +0800

    fenzhi

--- fegin-client/src/main/java/org/hxm/fegin/ctrl/T.java
+++ fegin-client/src/main/java/org/hxm/fegin/ctrl/T.java
@@ -5,6 +5,11 @@ package org.hxm.fegin.ctrl;
  * create at:  2019-11-06  16:33
  * @description: t
  */
-public class T   {
+
+public class T    {
+
+    public static void main(String[] args) {
+        
+    }
 }
 
